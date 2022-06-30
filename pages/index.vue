0741ec9d5f40ba62e7eb470b24bb1c7a563ed13c
thovagaushop <minhmeomeomun@gmail.com>
2022-06-30 11:40:54 +0700

    upcode l1

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ items }}</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    async getItems () {
+      const items = await this.$axios.get('http://localhost:3001/api/items')
+      this.items = items.data
+      console.log(items)
+    }
+  },
+  mounted () {
+    this.getItems()
+  }
+}
+</script>
